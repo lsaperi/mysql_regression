4e86a7aa8c0e5e917ec1731fc02d1b564b321687
lsaperi <lsaperi>
2025-08-21 11:28:46 -0300

    asdf; TaskID: 123

---
+++ TABLEs/Creation-Scripts/customer.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `customer` (
+  `idcustomer` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcustomer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
