f1a5c0c8bf11b330aebcb8790abbc6a09fbc0afe
lsaperi <lsaperi>
2025-08-21 11:49:12 -0300

    dsadsa

--- TABLEs/Creation-Scripts/customer.sql
+++ TABLEs/Creation-Scripts/customer.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `customer` (
   `idcustomer` int NOT NULL,
   `name` varchar(45) DEFAULT NULL,
+  `tel` varchar(45) NOT NULL,
   PRIMARY KEY (`idcustomer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
